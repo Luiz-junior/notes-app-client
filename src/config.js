c26845ed075f1a1f01ebdeac4e642a802e8d6f49
luiz-junior <junior.rodrigues0222@gmail.com>
2019-02-01 16:56:56 -0200

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes--app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://7svyfkqvqc.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_blALxKk4j",
+      APP_CLIENT_ID: "6upk249phbgm05qji7rv1gvb4h",
+      IDENTITY_POOL_ID: "us-east-1:e9f9e288-a032-4b85-895f-baf8bd600c35"
+    }
+  };
